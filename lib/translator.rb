0f7d405d571ac42cc34b7a875ca0c83885b90248
John Plantamura <plantamurajk@gmail.com>
2019-08-18 00:09:55 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,7 +21,7 @@ def get_japanese_emoticon(file_path, emoticon)
   dictionary['get_emoticon'].fetch(emoticon, "Sorry, that emoticon was not found")
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(file_path, emoticon)
+  dictionary = load_library(file_path)
 end
 
