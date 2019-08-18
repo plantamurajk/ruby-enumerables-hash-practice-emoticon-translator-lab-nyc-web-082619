40d433d0806c4f46ca02ca4826016cb6c3f0c25d
John Plantamura <plantamurajk@gmail.com>
2019-08-18 00:01:18 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,8 +4,15 @@ require 'yaml'
 
 def load_library(file_path)
   emoticons = YAML.load_file(file_path) 
-  puts emoticons
   dictionary = {'get_meaning' => {}, 'get_emoticon' => {}}
+  
+  emoticons.map do |meaning, faces|
+    english_emoticon = faces[0]
+    japanese_emoticon = faces[1]
+    dictionary['get_meaning'][japanese_emoticon] = meaning
+    dictionary['get_emoticon'][english_emoticon] = faces[1]
+  end
+    
   return dictionary
 end
 
